32d1c9c488749cd73ef015cab9c5310a859eeca9
Programador <fernando.eng.comp2019@gmail.com>
2021-11-21 16:14:45 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Java");
+        curso2.setDescricao("Descrição curso java");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setDate(LocalDate.now());
+
+        System.out.println(curso1 + ("\n"));
+        System.out.println(curso2 + ("\n"));
+        System.out.println(mentoria);
+
+    }
+}
